42fd0da9e53b68f9c3365f27a8445239d6d3af4c
Dilemma086 <Dilemma086@yandex.ru>
2025-02-19 12:20:15 +0300

    Изменил для теста

--- routes/categRoute.js
+++ routes/categRoute.js
@@ -7,37 +7,36 @@ categRoute.get('/:category', (req, res)=> {
     const category = req.params.category;
     
     const sql = `SELECT 
-        c.*,
-        ec.*,
-        t.*,
-        e.*,
-        e.name AS eventName,
-        i.image AS firstImageUrl,
-        MIN(t.date) AS ticketDate,
-        MIN(t.time) AS ticketTime
-      FROM 
-        category c
-      JOIN
-        eventcategory ec ON c.id = ec.categoryId
-      JOIN
-        tickets t ON ec.eventId = t.eventId
-      JOIN
-        events e ON ec.eventId = e.id
-      LEFT JOIN (
-        SELECT 
-              ei.eventId,
-              MIN(ei.imageId) AS firstImageId
-        FROM 
-              eventimage ei
-        GROUP BY 
-              ei.eventId
-        ) AS ei ON ec.eventId = ei.eventId
-        LEFT JOIN 
-            images i ON ei.firstImageId = i.id
-      WHERE 
-        url=?
-      GROUP BY 
-        ec.eventId`
+    MIN(c.id) AS categoryId,
+    MIN(c.name) AS categoryName,
+    ec.eventId,
+    MIN(t.date) AS ticketDate,
+    MIN(t.time) AS ticketTime,
+    MIN(e.name) AS eventName,
+    MIN(i.image) AS firstImageUrl
+FROM 
+    category c
+JOIN
+    eventcategory ec ON c.id = ec.categoryId
+JOIN
+    tickets t ON ec.eventId = t.eventId
+JOIN
+    events e ON ec.eventId = e.id
+LEFT JOIN (
+    SELECT 
+        ei.eventId,
+        MIN(ei.imageId) AS firstImageId
+    FROM 
+        eventimage ei
+    GROUP BY 
+        ei.eventId
+) AS ei ON ec.eventId = ei.eventId
+LEFT JOIN 
+    images i ON ei.firstImageId = i.id
+WHERE 
+    url=?
+GROUP BY 
+    ec.eventId;`
     connect.query(sql, [category], (err, result)=>{
       if (err) return res.json(err);
       return res.json(result);
